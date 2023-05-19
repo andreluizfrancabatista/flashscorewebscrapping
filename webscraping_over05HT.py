3d37626963c87ae8a0eb08b6a6306d54f0b9d583
André Luiz França Batista <andre.iftm@gmail.com>
2023-05-18 23:47:55 -0300

    Bugs corrigidos

--- webscraping_over05HT.py
+++ webscraping_over05HT.py
@@ -62,15 +62,14 @@ from selenium.webdriver.common.by import By
 # Com o WebDrive a gente consegue a pedir a página (URL)
 wd_Chrome.get("https://www.flashscore.com/") 
 time.sleep(2)
-# element = WebDriverWait(wd_Chrome, 30).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR,'button.calendar__navigation--tomorrow'))
-# )
 
 ## Para jogos do dia seguinte / Comentar essa linha para os jogos agendados de hoje 
 wd_Chrome.find_element(By.CSS_SELECTOR,'button.calendar__navigation--tomorrow').click()
 time.sleep(2)
-# element = WebDriverWait(wd_Chrome, 30).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR,'div.event__match--scheduled'))
+
+# Inserir aqui wait until element become clickable pra testar abrir os jogos fechados
+# element = WebDriverWait(wd_Chrome, 10).until(
+#     EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.event__info')) # div.event__info, span.event__expanderBlock, svg.event__expander--close
 # )
 
 # Pegando o ID dos Jogos
@@ -84,17 +83,17 @@ jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--scheduled')
 for i in jogos:
     id_jogos.append(i.get_attribute("id"))
 
-# Exemplo de ID de um jogo: 'g_1_Gb7buXVt'    
+# Exemplo de ID de um jogo: 'g_1_Gb7buXVt'
 id_jogos = [i[4:] for i in id_jogos]
 
 jogo = {'Date':[],'Time':[],'Country':[],'League':[],'Home':[],'Away':[],'golshtHome':[], 'totalHome':[], 'AvgHome':[], 'golshtAway':[], 'totalAway':[], 'AvgAway':[], 'pHome':[], 'pAway':[], 'Sum':[], 'Times':[]}
 
-#for link in tqdm(id_jogos, total=len(id_jogos)):
-for i, link in enumerate(id_jogos):
-    if(i>4):
-        break
+for link in tqdm(id_jogos, total=len(id_jogos)):
+# for i, link in enumerate(id_jogos):
+#     if(i>4):
+#         break
     wd_Chrome.get(f'https://www.flashscore.com/match/{link}/#/match-summary/') # English
-    time.sleep(2)
+    
     total, golsht = 0, 0
     golsHome, golsAway = 0, 0
     golshtAway, golshtHome = 0, 0
@@ -102,87 +101,72 @@ for i, link in enumerate(id_jogos):
     totalHome, totalAway = 0, 0
     pHome, pAway = 0, 0
     # Pegando as Informacoes Básicas do Jogo
-    # try:
-    # Inserir o wait aqui
-    element = WebDriverWait(wd_Chrome, 30).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR,'div.duelParticipant__home'))
-    )
-    Date = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[0]
-    Time = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[1]
-    Country = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country').text.split(':')[0]
-    League = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country')
-    League = League.find_element(By.CSS_SELECTOR,'a').text
-    Home = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__home')
-    LinkHome = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName')
-    LinkHome = LinkHome.find_element(By.TAG_NAME, 'a').get_attribute('href')
-    Home = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
-    Away = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__away')
-    LinkAway = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName')
-    LinkAway = LinkAway.find_element(By.TAG_NAME, 'a').get_attribute('href')
-    Away = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
-    
-    total, golsht = 0, 0
-    golsHome, golsAway = 0, 0
-    golshtAway, golshtHome = 0, 0
-    mediaGolsHTHome, mediaGolsHTAway = 0, 0
-    totalHome, totalAway = 0, 0
-    pHome, pAway = 0, 0
-    # Calcular a porcentagem de over 0,5 no HT de cada time
-    
-    links = [LinkHome, LinkAway]
-    for index, sublink in enumerate(links):
-        wd_Chrome.get(f'{sublink}results/') # English
-        # # Inserir o wait aqui
-        # element = WebDriverWait(wd_Chrome, 10).until(
-        #     EC.presence_of_element_located((By.CSS_SELECTOR,'div.event__part--home'))
-        # )
-        # time.sleep(10)
-        jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--static') #OR 'div.event__match--last'
+    try:
+        Date = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[0]
+        Time = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[1]
+        Country = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country').text.split(':')[0]
+        League = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country')
+        League = League.find_element(By.CSS_SELECTOR,'a').text
+        Home = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__home')
+        LinkHome = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName')
+        LinkHome = LinkHome.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        Home = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
+        Away = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__away')
+        LinkAway = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName')
+        LinkAway = LinkAway.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        Away = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
+        
         total, golsht = 0, 0
         golsHome, golsAway = 0, 0
         golshtAway, golshtHome = 0, 0
         mediaGolsHTHome, mediaGolsHTAway = 0, 0
         totalHome, totalAway = 0, 0
         pHome, pAway = 0, 0
-        print(f'{index}: {sublink}results/') # English
-        print(len(jogos))
-        for i in jogos:
-            try:
-                golsHome = i.find_element(By.CSS_SELECTOR, 'div.event__part--home').text
-                golsHome = int(golsHome[1:2])
-                golsAway = i.find_element(By.CSS_SELECTOR, 'div.event__part--away').text
-                golsAway = int(golsAway[1:2])
-                print(f'{golsHome}x{golsAway} ', end="")
-                
-                total += 1
-                if((golsHome+golsAway) > 0):
-                    golsht += 1
-            except:
-                print(f'?x? ', end="")
-                pass
-        print()
-        if(index==0):
-            pHome = golsht/total
-            totalHome = total
-            golshtHome = golsht
-            mediaGolsHTHome = golsHome/total
-            print(f'{pHome} {totalHome} {golshtHome} {mediaGolsHTHome} {golsHome}')
-        if(index==1):
-            pAway = golsht/total
-            totalAway = total
-            golshtAway = golsht
-            mediaGolsHTAway = golsAway/total
-            print(f'{pAway} {totalAway} {golshtAway} {mediaGolsHTAway} {golsAway}')
-        print()         
-    # except:
-    #     print(f'\nExcept: {link}')
-    #     pass
+        # Calcular a porcentagem de over 0,5 no HT de cada time
+        links = [LinkHome, LinkAway]
+        for index, sublink in enumerate(links):
+            wd_Chrome.get(f'{sublink}results/') # English
+            jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--static') #OR 'div.event__match--last'
+            total, golsht = 0, 0
+            gols = 0
+            # print(f'{index}: {sublink}results/') # English
+            for i in jogos:
+                try:
+                    golsHome = i.find_element(By.CSS_SELECTOR, 'div.event__part--home').text
+                    golsHome = int(golsHome[1:2])
+                    gols += golsHome
+                    golsAway = i.find_element(By.CSS_SELECTOR, 'div.event__part--away').text
+                    golsAway = int(golsAway[1:2])
+                    gols += golsAway
+                    # print(f'{golsHome}x{golsAway} ', end="")
+                    total += 1
+                    if((golsHome+golsAway) > 0):
+                        golsht += 1
+                except:
+                    # print(f'?x? ', end="")
+                    pass
+            # print()
+            if(index==0):
+                pHome = golsht/total
+                totalHome = total
+                golshtHome = golsht
+                mediaGolsHTHome = gols/total
+                # print(f'pHome:{pHome*100:.2f} jogos:{totalHome} jogosComGolHT:{golshtHome} média:{mediaGolsHTHome:.2f} gols:{gols}')
+            if(index==1):
+                pAway = golsht/total
+                totalAway = total
+                golshtAway = golsht
+                mediaGolsHTAway = gols/total
+                # print(f'pAway:{pAway*100:.2f} jogos:{totalAway} jogosComGolHT:{golshtAway} média:{mediaGolsHTAway:.2f} gols:{gols}')
+            # print()         
+    except:
+        print(f'\nExcept: {link}')
+        pass
 
     # print(Date,Time,Country,League,Home,Away,Odds_H,Odds_D,Odds_A) 
     # print(f'{Date}, {Time}, {Country}, {League}\n{Home} {pHome*100:.2f} x {pAway*100:.2f} {Away}\n') 
 
     # Colocar tudo dentro do df pra salvar no csv
-
     jogo['Date'].append(Date.replace(".", "/"))
     jogo['Time'].append(Time)
     jogo['Country'].append(Country.replace(";", "-"))
@@ -202,7 +186,7 @@ for i, link in enumerate(id_jogos):
         )
     jogo['Times'].append(
         str(round((round(pHome, 4) * round(pAway, 4)), 4)).replace(".", ",")
-    )
+        )
     
 
 df = pd.DataFrame(jogo)
