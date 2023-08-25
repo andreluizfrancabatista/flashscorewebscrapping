3bb0dacc8e19f206c969b546e65e3c35b56ca78a
André Luiz França Batista <andre.iftm@gmail.com>
2023-08-24 22:36:41 -0300

    26-08-2023

--- last20_montecarlo.py
+++ last20_montecarlo.py
@@ -12,31 +12,25 @@ Criado por:
 
 # Instalando as Bibliotecas e as Dependências
 """
+"""# Importando as Bibliotecas"""
 
-# Instalando o Selenium
-# !pip install selenium
-
-# Atualizando o Ubuntu para executar corretamento o apt-install
-# !apt-get update
-
-# Instalando o ChromeDrive e Trazendo ele para a Pasta Local
-# !apt install chromium-chromedriver
-
-# !cp /usr/lib/chromium-browser/chromedriver /usr/bin
-import sys
-sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from tqdm import tqdm
+import pandas as pd
+import time
 import os
 import numpy as np
-"""# Configuração do Web-Driver"""
+import math
+import sys
+sys.path.insert(0, '/usr/lib/chromium-browser/chromedriver')
 
+# Configuração do Web-Driver
 # Utilizando o WebDriver do Selenium
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 # Instanciando o Objeto ChromeOptions
 options = webdriver.ChromeOptions()
 
@@ -49,17 +43,9 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--disable-crash-reporter')
 options.add_argument('--log-level=3')
 
-
 # Criação do WebDriver do Chrome
-# wd_Chrome = webdriver.Chrome('chromedriver',options=options)
-wd_Chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-
-"""# Importando as Bibliotecas"""
-
-import pandas as pd
-import time
-from tqdm import tqdm
+wd_Chrome = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=options)
 
 # Dict com os dias da semana e as siglas
 week = {
@@ -72,19 +58,23 @@ week = {
     "SA": "Saturday"
 }
 
-partidas = 20
+# Distribuição Poisson
+def poisson(x, mean):
+  return round(((math.exp(-mean) * (pow(mean, x)))/(math.factorial(x)) *100),2)
+
+partidas = 30
 
 """# Iniciando a Raspagem de Dados"""
 
 # Com o WebDrive a gente consegue a pedir a página (URL)
-wd_Chrome.get("https://www.flashscore.com/") 
+wd_Chrome.get("https://www.flashscore.com/")
 time.sleep(2)
 
-## Para jogos do dia seguinte / Comentar essa linha para os jogos agendados de hoje 
+# Para jogos do dia seguinte / Comentar essa linha para os jogos agendados de hoje
 # wd_Chrome.find_element(By.CSS_SELECTOR,'button.calendar__navigation--tomorrow').click()
 # time.sleep(2)
 
-next_day = wd_Chrome.find_elements(By.CSS_SELECTOR,'button.calendar__navigation--tomorrow')
+next_day = wd_Chrome.find_elements(By.CSS_SELECTOR, 'button.calendar__navigation--tomorrow')
 for button in next_day:
     wd_Chrome.execute_script("arguments[0].click();", button)
 time.sleep(2)
@@ -100,10 +90,10 @@ for button in display_matches:
 
 # Pegando o ID dos Jogos
 id_jogos = []
-## Para jogos agendados (próximos)
-jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--scheduled')
+# Para jogos agendados (próximos)
+jogos = wd_Chrome.find_elements(By.CSS_SELECTOR, 'div.event__match--scheduled')
 
-## Para jogos ao vivo (live)
+# Para jogos ao vivo (live)
 # jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--live')
 
 for i in jogos:
@@ -113,113 +103,169 @@ for i in jogos:
 id_jogos = [i[4:] for i in id_jogos]
 
 jogo = {
-    'Date':[],'Time':[],'Country':[],'League':[],'Home':[],'Away':[],
-    'totalHome':[], 'totalAway':[], 
-    'avgHome':[], 'sdHome':[], 'avgAway':[], 'sdAway':[], 'avgSum':[],
-    '0x0':[], '1x0':[], '2x0':[], '1x1':[], '0x1':[], '0x2':[], 'U25':[]
+    'Date': [], 'Time': [], 'Country': [], 'League': [], 'Home': [], 'Away': [],
+    'golsHome': [], 'jogosHome': [], 'golsAway': [], 'jogosAway': [],
+    'avgHome': [], 'sdHome': [], 'avgAway': [], 'sdAway': [], 'avgSum': [],
+    '0x0': [], '1x0': [], '2x0': [], '1x1': [], '0x1': [], '0x2': [], 'U25': []
 }
 
 # Lista de países vetados
 Countries = ["RUSSIA", "BELARUS", "UKRAINE"]
 
 for x, link in enumerate(tqdm(id_jogos, total=len(id_jogos))):
-    wd_Chrome.get(f'https://www.flashscore.com/match/{link}/#/match-summary/') # English
+    # if(x>11):
+    #     break
+    wd_Chrome.get(f'https://www.flashscore.com/match/{link}/#/match-summary/')  # English
 
     # Checar se o país está na lista de vetados
-    Country = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country').text.split(':')[0]
+    Country = wd_Chrome.find_element(By.CSS_SELECTOR, 'span.tournamentHeader__country').text.split(':')[0]
     if Country in Countries:
         continue
-    
-    total, golsht = 0, 0
-    golsHome, golsAway = 0, 0
-    golshtAway, golshtHome = 0, 0
-    golsArrayHome = []
-    golsArrayAway = []
-    mediaGolsHTHome, mediaGolsHTAway = 0, 0
-    sdHome, sdAway = 0, 0
-    totalHome, totalAway = 0, 0
-    pHome, pAway = 0, 0
+
     # Pegando as Informacoes Básicas do Jogo
     try:
-        Date = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[0]
-        Time = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__startTime').text.split(' ')[1]
-        Country = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country').text.split(':')[0]
-        League = wd_Chrome.find_element(By.CSS_SELECTOR,'span.tournamentHeader__country')
-        League = League.find_element(By.CSS_SELECTOR,'a').text
-        Home = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__home')
-        LinkHome = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName')
+        Date = wd_Chrome.find_element(By.CSS_SELECTOR, 'div.duelParticipant__startTime').text.split(' ')[0]
+        Time = wd_Chrome.find_element(By.CSS_SELECTOR, 'div.duelParticipant__startTime').text.split(' ')[1]
+        Country = wd_Chrome.find_element(By.CSS_SELECTOR, 'span.tournamentHeader__country').text.split(':')[0]
+        League = wd_Chrome.find_element(By.CSS_SELECTOR, 'span.tournamentHeader__country')
+        League = League.find_element(By.CSS_SELECTOR, 'a').text
+        Home = wd_Chrome.find_element(By.CSS_SELECTOR, 'div.duelParticipant__home')
+        LinkHome = Home.find_element(By.CSS_SELECTOR, 'div.participant__participantName')
         LinkHome = LinkHome.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        Home = Home.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
-        Away = wd_Chrome.find_element(By.CSS_SELECTOR,'div.duelParticipant__away')
-        LinkAway = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName')
+        Home = Home.find_element(By.CSS_SELECTOR, 'div.participant__participantName').text
+        Away = wd_Chrome.find_element(By.CSS_SELECTOR, 'div.duelParticipant__away')
+        LinkAway = Away.find_element(By.CSS_SELECTOR, 'div.participant__participantName')
         LinkAway = LinkAway.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        Away = Away.find_element(By.CSS_SELECTOR,'div.participant__participantName').text
+        Away = Away.find_element(By.CSS_SELECTOR, 'div.participant__participantName').text
         
-        total, golsht = 0, 0
-        golsHome, golsAway = 0, 0
-        golshtAway, golshtHome = 0, 0
-        mediaGolsHTHome, mediaGolsHTAway = 0, 0
+        # Verificar se o nome do time tem (xxx) no final e remover
+        if ")" in Home:
+            pos = Home.find('(')
+            Home = Home[:pos-1]
+        if ")" in Away:
+            pos = Away.find('(')
+            Away = Away[:pos-1]
+
+        # Acessar a página de /results/ de cada time (Home e Away)
+        ### HOME ###
+        # Inicializando variáveis de contagem
+        gols = 0
+        total = 0
         golsArrayHome = []
-        golsArrayAway = []
-        sdHome, sdAway = 0, 0
-        totalHome, totalAway = 0, 0
-        pHome, pAway = 0, 0
-        
-        links = [LinkHome, LinkAway]
-        for index, sublink in enumerate(links):
-            wd_Chrome.get(f'{sublink}results/') # English
-            jogos = wd_Chrome.find_elements(By.CSS_SELECTOR,'div.event__match--static') #OR 'div.event__match--last'
-            total, golsht = 0, 0
-            gols = 0
-            golsArray = []
-            # print(f'{index}: {sublink}results/') # English
-            for i in jogos:
-                try:
-                    resultHome = i.find_element(By.CSS_SELECTOR, 'div.event__participant--home').text
-                    # resultAway = i.find_element(By.CSS_SELECTOR, 'div.event__participant--away').text
-                    if(Home == resultHome):
-                        golsHome = i.find_element(By.CSS_SELECTOR, 'div.event__score--home').text
+        wd_Chrome.get(f'{LinkHome}results/')  # English
+        # OR 'div.event__match--last'
+        jogos = wd_Chrome.find_elements(By.CSS_SELECTOR, 'div.event__match--static')
+        for i in jogos:
+            try:
+                resultHome = i.find_element(By.CSS_SELECTOR, 'div.event__participant--home').text
+                # Verificar se o nome do time tem (xxx) no final e remover
+                if ")" in resultHome:
+                    pos = resultHome.find('(')
+                    resultHome = resultHome[:pos-1]
+                if (Home == resultHome):
+                    golsHome = i.find_element(By.CSS_SELECTOR, 'div.event__score--home').text
+                    if(golsHome != "-"):
                         golsHome = int(golsHome)
                         gols += golsHome
-                        total += 1 # total de jogos que o time Home fez em casa dentro da lista de 30 jogos
+                        total += 1  # total de jogos que o time Home fez em casa dentro da lista de 30 jogos
                         golsArrayHome.append(golsHome)
-                        if(total>=partidas):
-                            break
-                    
-                    # golsAway = i.find_element(By.CSS_SELECTOR, 'div.event__score--away').text
-                    # golsAway = int(golsAway)
-                    # gols += golsAway
-                    # print(f'{golsHome}x{golsAway} ', end="")
-                    # if((golsHome+golsAway) < 2.5):
-                    #     golsht += 1
-                except:
-                    # print(f'?x? ', end="")
-                    pass
-            # print()
-            if(index==0):
-                pHome = golsht/total
-                totalHome = total
-                golshtHome = golsht
-                mediaGolsHTHome = gols/total
-                golsArrayHome = np.array(golsArrayHome)
-                sdHome = golsArray.std() # Calcular o SD de golsArray
-                # print(f'{Home} x {Away} --> {golsArray} --> {sdHome}')
-                # print(f'pHome:{pHome*100:.2f} jogos:{totalHome} jogosComGolHT:{golshtHome} média:{mediaGolsHTHome:.2f} gols:{gols}')
-            if(index==1):
-                pAway = golsht/total
-                totalAway = total
-                golshtAway = golsht
-                mediaGolsHTAway = gols/total
-                golsArray = np.array(golsArray)
-                sdAway = golsArray.std() # Calcular o SD de golsArray
-                # print(f'pAway:{pAway*100:.2f} jogos:{totalAway} jogosComGolHT:{golshtAway} média:{mediaGolsHTAway:.2f} gols:{gols}')
-            # print()       
+                    if(total >= partidas):
+                        break
+            except Exception as error:
+                print(f'\n{Home} x {LinkHome}: {error}')
+
+        # Calcular o total de jogos, a média e desvio padrão
+        golsHome = gols
+        jogosHome = total
+        mediaGolsHome = golsHome/jogosHome
+        golsArrayHome = np.array(golsArrayHome)
+        sdHome = golsArrayHome.std()  # Calcular o SD de golsArrayHome
+
+        ### AWAY ###
+        # Inicializando variáveis de contagem
+        gols = 0
+        total = 0
+        golsArrayAway = []
+        wd_Chrome.get(f'{LinkAway}results/')  # English
+        # OR 'div.event__match--last'
+        jogos = wd_Chrome.find_elements(
+            By.CSS_SELECTOR, 'div.event__match--static')
+        for i in jogos:
+            try:
+                resultAway = i.find_element(By.CSS_SELECTOR, 'div.event__participant--away').text
+                # Verificar se o nome do time tem (xxx) no final e remover
+                if ")" in resultAway:
+                    pos = resultAway.find('(')
+                    resultAway = resultAway[:pos-1]
+                if (Away == resultAway):
+                    golsAway = i.find_element(By.CSS_SELECTOR, 'div.event__score--away').text
+                    if(golsAway != "-"):
+                        golsAway = int(golsAway)
+                        gols += golsAway
+                        total += 1  # total de jogos que o time Away fez fora de casa dentro da lista de 30 jogos
+                        golsArrayAway.append(golsAway)
+                    if(total >= partidas):
+                        break
+            except Exception as error:
+                print(f'\n{Away} x {LinkAway}: {error}')
+
+        # Calcular o total de jogos, a média e desvio padrão
+        golsAway = gols
+        jogosAway = total
+        mediaGolsAway = golsAway/jogosAway
+        golsArrayAway = np.array(golsArrayAway)
+        sdAway = golsArrayAway.std()  # Calcular o SD de golsArrayAway
+
+        # Calcular as probabilidades pelo método de simulações de Monte Carlo
+        mean1 = mediaGolsHome
+        mean2 = mediaGolsAway
+        ngols = 6
+
+        probs = {
+            "Home":[],
+            "Away":[]
+        }
+
+        probs["Home"] = [poisson(x, mean1) for x in range(ngols+1)]
+        probs["Away"] = [poisson(x, mean2) for x in range(ngols+1)]
+
+        g1 = np.arange(ngols+1)
+        s1 = pd.Series(probs["Home"])
+        s2 = pd.Series(probs["Away"])
+
+        tuples1 = list(zip(g1, s1))
+        tuples2 = list(zip(g1, s2))
+
+        index1 = pd.MultiIndex.from_tuples(tuples1, names=["gols", "prob"])
+        index2 = pd.MultiIndex.from_tuples(tuples2, names=["gols", "prob"])
+
+        df = pd.DataFrame(1, index=index1, columns=index2)
+
+        s1 = pd.Series(df.index.get_level_values(1))
+        s2 = pd.Series(df.columns.get_level_values(1))
+        df1 = pd.DataFrame(1, index=s1.index, columns=s2.index)
+        new = round((df1.multiply(s1, axis='index') * s2 / 100),2)
+        #
+        # Conferir aqui se os resultados não estão invertidos
+        #
+        r0x0 = new[0][0] # 0x0
+        r0x1 = new[0][1] # 0x1
+        r0x2 = new[0][2] # 0x2
+        r1x1 = new[1][1] # 1x1
+        r1x0 = new[1][0] # 1x0
+        r2x0 = new[2][0] # 2x0
+        rU25 = r0x0 + r0x1 + r0x2 + r1x1 + r1x0 + r2x0
+
     except Exception as error:
         print(f'\n{error}\nExcept: {Home} x {Away} - {link}')
         pass
 
-    # print(Date,Time,Country,League,Home,Away,Odds_H,Odds_D,Odds_A) 
-    # print(f'{Date}, {Time}, {Country}, {League}\n{Home} {pHome*100:.2f} x {pAway*100:.2f} {Away}\n') 
+    # jogo = {
+    #     'Date': [], 'Time': [], 'Country': [], 'League': [], 'Home': [], 'Away': [],
+    #     'golsHome': [], 'jogosHome': [], 'golsAway': [], 'jogosAway': [],
+    #     'avgHome': [], 'sdHome': [], 'avgAway': [], 'sdAway': [], 'avgSum': [],
+    #     '0x0': [], '1x0': [], '2x0': [], '1x1': [], '0x1': [], '0x2': [], 'U25': []
+    # }
 
     # Colocar tudo dentro do df pra salvar no csv
     jogo['Date'].append(Date.replace(".", "/"))
@@ -228,36 +274,44 @@ for x, link in enumerate(tqdm(id_jogos, total=len(id_jogos))):
     jogo['League'].append(League.replace(";", "-"))
     jogo['Home'].append(Home.replace(";", "-"))
     jogo['Away'].append(Away.replace(";", "-"))
-    jogo['jogosHome'].append(golshtHome)
-    jogo['totalHome'].append(totalHome)
-    jogo['jogosAway'].append(golshtAway)
-    jogo['totalAway'].append(totalAway)
-    jogo['pHome'].append(str(round(pHome, 4)).replace(".", ","))
-    jogo['pAway'].append(str(round(pAway, 4)).replace(".", ","))
-    jogo['pSum'].append(
-        str(round((round(pHome, 4) + round(pAway, 4)), 4)).replace(".", ",")
-        )
-    jogo['avgHome'].append(str(round(mediaGolsHTHome, 4)).replace(".", ","))
+    jogo['golsHome'].append(golsHome)
+    jogo['jogosHome'].append(jogosHome)
+    jogo['golsAway'].append(golsAway)
+    jogo['jogosAway'].append(jogosAway)
+    
+    jogo['0x0'].append(str(round(r0x0, 4)).replace(".", ","))
+    jogo['0x1'].append(str(round(r0x1, 4)).replace(".", ","))
+    jogo['0x2'].append(str(round(r0x2, 4)).replace(".", ","))
+    jogo['1x1'].append(str(round(r1x1, 4)).replace(".", ","))
+    jogo['1x0'].append(str(round(r1x0, 4)).replace(".", ","))
+    jogo['2x0'].append(str(round(r2x0, 4)).replace(".", ","))
+    jogo['U25'].append(str(round(rU25, 4)).replace(".", ","))
+    
+    jogo['avgHome'].append(str(round(mediaGolsHome, 4)).replace(".", ","))
     jogo['sdHome'].append(str(round(sdHome, 4)).replace(".", ","))
-    jogo['avgAway'].append(str(round(mediaGolsHTAway, 4)).replace(".", ","))
+    jogo['avgAway'].append(str(round(mediaGolsAway, 4)).replace(".", ","))
     jogo['sdAway'].append(str(round(sdAway, 4)).replace(".", ","))
     jogo['avgSum'].append(
-        str(round((round(mediaGolsHTHome, 4) + round(mediaGolsHTAway, 4)), 4)).replace(".", ",")
-        )
-    
+        str(round(
+            (round(mediaGolsHome, 4) + round(mediaGolsAway, 4)), 4)
+            ).replace(".", ",")
+    )
+
 
 # Para atualizar o CSV a cada 'n' interações.
 # Colocar esse código abaixo dentro do loop acima (subir uma identação)
 # Colocar uma condição para a cada 'n' interações do loop, realizar a escrita no arquivo
 df = pd.DataFrame(jogo)
 
-# Drop rows totalHome < 15 or totalAway < 15
-filtered = df[ (df['totalHome'] < 15) | (df['totalAway'] < 15) ].index
-df.drop(filtered , inplace=True)
+# Drop rows jogosHome < 15 or jogosAway < 15
+filtered = df[(df['jogosHome'] < 15) | (df['jogosAway'] < 15)].index
+df.drop(filtered, inplace=True)
 
-df = df.sort_values(by=['pSum'], ascending=False)
+df = df.sort_values(by=['U25'], ascending=False)
 df.reset_index(inplace=True, drop=True)
 df.index = df.index.set_names(['Nº'])
 df = df.rename(index=lambda x: x + 1)
-filename = "lista_de_jogos/jogos_do_dia_"+Date.replace(".", "_")+"_last"+str(partidas)+"_U25FT_com_SD_com_MonteCarlo.csv"
+filename = "lista_de_jogos/jogos_do_dia_" + \
+    Date.replace(".", "_")+"_last"+str(partidas) + \
+    "_U25FT_com_SD_com_MonteCarlo.csv"
 df.to_csv(filename, sep=";")
